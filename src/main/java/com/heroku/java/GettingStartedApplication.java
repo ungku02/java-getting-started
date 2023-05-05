b389d4e122ac290d049c979c7c201ce0c4f847a4
UNGKU ZAINAB BT UNGKU BADRUL SHAH <129198293+ungku02@users.noreply.github.com>
2023-05-05 21:29:31 +0800

    GettingStartedApplication.java

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -10,6 +10,20 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
+import javax.measure.unit.SI;
+
+@GetMapping("/convert")
+String convert(Map<String, Object> model) {
+    RelativisticModel.select();
+    var energy = Amount.valueOf("12 GeV");
+
+    model.put("result", "E=mc^2: " + energy + " = " + energy.to(SI.KILOGRAM));
+    return "convert";
+}
+
+
 
 @SpringBootApplication
 @Controller
